830e121c7cb37eee73e5a67f74b29f8292a0618e
José Isaac <joseisaacnascimento@gmail.com>
2026-02-17 15:58:39 -0300

    feat(scraper): add function to accept_cookies

--- src/main.py
+++ src/main.py
@@ -1,10 +1,11 @@
 from src.browser import init_browser, close_browser
-from src.scraper import open_indeed
+from src.scraper import open_indeed, accept_cookies
 
 
 def main():
     browser = init_browser()
     open_indeed(browser)
+    accept_cookies(browser)
     close_browser(browser)
 
 
--- src/scraper.py
+++ src/scraper.py
@@ -1,4 +1,7 @@
 from src.logger import init_logger
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 logger = init_logger()
 
@@ -7,3 +10,11 @@ def open_indeed(browser):
     url = "https://br.indeed.com/jobs?q=&l=Brasil&from=searchOnHP%2Cwhereautocomplete&vjk=e2b04a667ea7ec82"
     logger.info(f"Acessando {url}")
     browser.get(url)
+
+
+def accept_cookies(browser):
+    wait = WebDriverWait(browser, 10)
+    wait.until(
+        EC.element_to_be_clickable((By.ID, "onetrust-accept-btn-handler"))
+    ).click()
+    logger.info("Aceitando cookies")
