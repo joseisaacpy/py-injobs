d93ed2cf6d08ae4bd002b3dfceb437cea54256c0
José Isaac <joseisaacnascimento@gmail.com>
2026-02-17 11:32:43 -0300

    feat(browser): implement browser init and close functions

---
+++ src/scraper/browser.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+from src.logger import init_logger
+
+logger = init_logger()
+
+def init_browser():
+    options = webdriver.ChromeOptions()
+    options.add_argument("--start-maximized")
+    
+    logger.info("Iniciando o navegador")
+    
+    browser = webdriver.Chrome(options=options)
+    return browser
+
+
+def close_browser(browser):
+    if browser:
+        logger.info("Fechando o navegador")
+        browser.quit()
