27435235fb466937eb9d21b5dc17ee23bcec9325
José Isaac <joseisaacnascimento@gmail.com>
2026-02-21 20:49:52 -0300

    refactor(main): add try/finally to close browser

--- src/main.py
+++ src/main.py
@@ -1,13 +1,27 @@
 from src.browser import init_browser, close_browser
-from src.scraper import open_indeed, accept_cookies, get_card_jobs
+from src.scraper import (
+    open_indeed,
+    accept_cookies,
+    get_card_jobs,
+    collect_jobs,
+    save_to_csv,
+)
 
 
 def main():
     browser = init_browser()
-    open_indeed(browser)
-    accept_cookies(browser)
-    cards = get_card_jobs(browser)
-    close_browser(browser)
+
+    try:
+        open_indeed(browser)
+        accept_cookies(browser)
+
+        cards = get_card_jobs(browser)
+        jobs = collect_jobs(cards)
+
+        save_to_csv(jobs)
+
+    finally:
+        close_browser(browser)
 
 
 if __name__ == "__main__":
